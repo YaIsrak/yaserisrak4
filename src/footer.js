99c6f89f95cc8384f40a8f27c910fe7fe53b01cc
YaIsrak <israkdreamstar@gmail.com>
2021-04-18 15:49:55 +0600

    Innital Commit

---
+++ src/footer.js
@@ -0,0 +1,23 @@
+const footer = (
+  <div class="container">
+    <div class="social-icon text-center">
+      <a href="https://www.facebook.com/yaser.arafat.israk">
+        <i class="fab fa-facebook-f text-white"></i>
+      </a>
+      <a href="https://www.instagram.com/_yaser.israk_">
+        <i class="fab fa-instagram text-white"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/israk">
+        <i class="fab fa-linkedin-in text-white"></i>
+      </a>
+      <a href="https://www.youtube.com/channel/UC1gsOgkQq6EvgL-IP8QeHnQ?view_as=subscriber">
+        <i class="fab fa-youtube text-white"></i>
+      </a>
+    </div>
+    <div class="credit text-center">
+      Copyright ©2021 Israk . All rights reserved.
+    </div>
+  </div>
+);
+
+ReactDOM.render(footer, document.querySelector("footer"));
